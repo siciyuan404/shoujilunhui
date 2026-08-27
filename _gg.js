66f226df1db4970f4b8465e17f6b04e6641b35f7
siciyuan404 <siciyuan404@users.noreply.github.com>
2026-08-28 01:29:46 +0800

    补录谷歌手机(Pixel)机型图片与参数 8台

---
+++ _gg.js
@@ -0,0 +1,10 @@
+﻿const { DatabaseSync } = require('node:sqlite');
+const db = new DatabaseSync('F:/git/shoujilunhui/server/db/phone.db', { readOnly: true });
+const r = db.prepare("SELECT model FROM models WHERE brand='谷歌手机' ORDER BY model").all();
+console.log('谷歌手机 总数:', r.length);
+r.forEach(x=>console.log(x.model));
+console.log('---其他品牌剩余---');
+for (const b of ['其他','乐视','鼎桥','智选']) {
+  const rr = db.prepare("SELECT model FROM models WHERE brand=?").all(b);
+  console.log(b, rr.length, rr.map(x=>x.model).join(' / '));
+}
