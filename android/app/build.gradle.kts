835806be8b8023a31b0d8f35cdcfadc4f58850a7
siciyuan404 <siciyuan404@users.noreply.github.com>
2026-09-06 13:39:35 +0800

    版本 25 / 1.12.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.shoujilunhui.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 24
-        versionName = "1.12.0"
+        versionCode = 25
+        versionName = "1.12.1"
     }
 
     signingConfigs {
